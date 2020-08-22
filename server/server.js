30a66528e5f658a9f8de9bf1610135e9eb974872
Jae Yoo <pppjqqq@gmail.com>
2020-08-21 21:41:23 -0700

    proxy server running on aws

--- server/server.js
+++ server/server.js
@@ -1,43 +1,33 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const PORT = 3535;
+const PORT = 3002;
 const path = require('path')
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-// app.use(express.json());
-
 app.get('/sidebar/summary', function (req, res) {
-  axios.get('http://127.0.0.1:3380/sidebar/summary')
+  axios.get('http://54.176.46.102:3380/sidebar/summary')
     .then(response => {
-      // console.log(response.data);
       res.send(response.data)
     })
     .catch((response) => {console.log('catch', response.data)})
 })
 
 app.get('/kix', function (req, res) {
-  axios.get('http://127.0.0.1:5291/kix')
+  axios.get('http://ec2-54-176-93-251.us-west-1.compute.amazonaws.com/kix')
     .then(response => {
-      // console.log(response.data);
       res.status(200).send(response.data)
     })
     .catch((response) => {console.log('catch', response.data)})
 })
 
 app.get('/api/reviews', function (req, res) {
-  axios.get('http://127.0.0.1:3001/api/reviews')
+  axios.get('http://52.14.202.194:3001/api/reviews')
     .then(response => {
-      // console.log(response.data);
       res.status(200).send(response.data)
     })
     .catch((response) => {console.log('catch', response.data)})
 })
-
-
-// app.post('/sidebar/summary', (req, res) => {
-
-// })
 
 app.listen(PORT, () => console.log('Server is listening at port', PORT))
